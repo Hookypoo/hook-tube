a4aa73138b5323c03febb2abe276111e9903627c
Hookypoo <forde.hugh@yahoo.com>
2020-11-13 22:16:08 -0400

    added search page avatar,name,verify icon

--- src/ChannelRow.js
+++ src/ChannelRow.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import "./ChannelRow.css"
+import React from 'react';
+import "./ChannelRow.css";
+import VerifiedIcon from '@material-ui/icons/CheckCircleOutlineOutlined';
 import Avatar from "@material-ui/core/Avatar";
 
-function ChannelRow(image, Channel, verified, Subs, noOfVideoes, description) {
+function ChannelRow({ image, channel, verified, subs, noOfVideoes, description }) {
     return (
         <div className="ChannelRow">
-
+            <Avatar className="channelRow__logo" alt={channel} src={image} />
+            <div className="channelRow__text">
+                <h4>
+                    {channel} {verified && <VerifiedIcon />}
+                </h4>
+            </div>
         </div>
-    )
+    );
 }
 
 export default ChannelRow
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -14,8 +14,8 @@ function SearchPage() {
 
             <ChannelRow
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRcfxxRI4jXAzgxe8UqTZVsdH8BJU52o8ZEVg&usqp=CAU"
-                Channel="Hookypoo's Place"
-                verified=""
+                channel="Hookypoo's Place"
+                verified
                 subs="700K"
                 noOfVideos={382}
                 description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper nisl nec varius iaculis. Suspendisse luctus dui ac lobortis volutpat. Nam varius nisi quam, imperdiet imperdiet enim gravida in. Donec vel nisi sit amet sapien venenatis varius consequat a ante. Praesent non lacus sed elit interdum sollicitudin. Nam finibus metus."
